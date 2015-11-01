abe6a73782cb924e3c02b7088ac135208dbd32aa
Ruslan Shaydulin <ruslan@l2sh.ru>
2015-11-01 20:00:51 +0300

    Initial Commit

---
+++ SMMailCoreKit/SMMailCoreKit.h
@@ -0,0 +1,19 @@
+//
+//  SMMailCoreKit.h
+//  SMMailCoreKit
+//
+//  Created by Руслан on 01.11.15.
+//  Copyright © 2015 MIPT. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SMMailCoreKit.
+FOUNDATION_EXPORT double SMMailCoreKitVersionNumber;
+
+//! Project version string for SMMailCoreKit.
+FOUNDATION_EXPORT const unsigned char SMMailCoreKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SMMailCoreKit/PublicHeader.h>
+
+
